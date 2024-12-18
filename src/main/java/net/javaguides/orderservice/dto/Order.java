85b24a708b7ab00c3c906ebda19794eb0022513a
nadiah-m <nadiah.malik@Hotmail.com>
2024-12-18 18:49:44 +0800

    add setters and getters

--- src/main/java/net/javaguides/orderservice/dto/Order.java
+++ src/main/java/net/javaguides/orderservice/dto/Order.java
@@ -12,4 +12,12 @@ public class Order {
     private String name;
     private int qty;
     private double price;
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
 }
--- src/main/java/net/javaguides/orderservice/dto/OrderEvent.java
+++ src/main/java/net/javaguides/orderservice/dto/OrderEvent.java
@@ -11,4 +11,29 @@ public class OrderEvent {
     private String status; // pending, progress, completed
     private String message;
     private Order order;
+
+    // Getters and setters
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
